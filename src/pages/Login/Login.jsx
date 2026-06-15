2b98c2889f375b2195f191c9be11ca63edb99faa
BrayanMateus5 <mateusbrayan73@gmail.com>
2026-06-15 00:58:28 -0300

    style: finallize style from login screen

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,38 @@
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
+import logo from '../../assets/nexion-logo.png';
 import './Login.css';
 
 
 export default function Login() {
     return (
-        <div className="login-container">
-            <h1>Entrar no Nexion</h1>
+        <div className="login-layout-container">
 
-            <div className="p-fluid">
-                <div className="p-field">
-                    <label htmlFor="email">E-mail</label>
-                    <InputText id="email" placeholder="Digite seu e-mail" />
+            {/*Lado da marca */}
+            <div className="brand-section">
+                <img src={logo} alt="Logo da Nexion" className="logo-image" />
+                <h1 className="brand-name">Nexion</h1>
+            </div>
+
+            {/*Lado do formulário */}
+            <div className="form-section">
+                <div className="p-fluid">
+                    <h2>Entrar no Nexion</h2>
+
+                    <div className="p-field">
+                        <label htmlFor="email">Email</label>
+                        <InputText id="email" type="text" placeholder="Digite seu email" />
+                    </div>
+
+                    <div className="p-field">
+                        <label htmlFor="password">Senha</label>
+                        <Password id="password" placeholder="Digite sua senha" feedback={false} toggleMask />
+                    </div>
+
+                    <Button label="Entrar" className="nexion-btn" />
+
                 </div>
-
-                <div className="p-field">
-                    <label htmlFor="password">Senha</label>
-                    <Password id="password" placeholder="Digite sua senha" feedback={false} />  {/*desativar a dica de senha*/}
-                </div>
-
-                <Button label="Acessar sistema" className="p-mt-2" />
             </div>
         </div>
     );
