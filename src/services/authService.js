f8e74ef62ac7e8f2159473e6f7a4e6b20b6133ee
BrayanMateus5 <mateusbrayan73@gmail.com>
2026-06-20 12:30:50 -0300

    feat(login): create authentication service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,29 +1,41 @@
 const USUARIO_FALSO = [
-    { id: 1, nome: 'Brayan Mateus', email: 'braya@nexion.com', senha: '123456' },
+    { id: 1, nome: 'Brayan Mateus', email: 'braya@nexion.com', password: '123456' },
 ];
 
-export function login(email, senha) {
+export function login(email, password) {
+
     return new Promise((aceita, rejeita) => {
-        setTimeout(() =>
+        setTimeout(() => {
+
             const usuario = USUARIO_FALSO.find(
-            (u) => u.email === email && u.senha === senha
-        )
-    );
+                (u) => u.email === email && u.password === password
+            );
 
-        if (usuario) {
-            aceita({
-                token: 'nexion-token-falso-123', usuario: { id: usuario.id, nome: usuario.nome, email: usuario.email },
-            });
-            //aqui ele valida o email inserido
+            if (usuario) {
+                aceita({
+                    token: 'nexion-token-falso-123', usuario: { id: usuario.id, nome: usuario.nome, email: usuario.email },
+                });
+                //aqui ele valida o email inserido
 
-        } else {
-            rejeita(new Error('E-mail e senha incorretos'));
-        }
-        //aqui ele rejeita dando erro e mensagem
-    }, 1500);
-});
+            } else {
+                rejeita(new Error('E-mail e senha incorretos'));
+            }
+            //aqui ele rejeita dando erro e mensagem
+        }, 1500);
+    });
 }
 
-export function cadastrar(nome, email, senha) {
-    return new Promise
+export function cadastrar(nome, email, password) {
+    return new Promise((aceita, rejeita) => {
+        setTimeout(() => {
+
+            const jaExiste = USUARIO_FALSO.find((u) => u.email === email);
+
+            if (jaExiste) {
+                rejeita(new Error('E-mail já cadastrado no sistema !'));
+            } else {
+                aceita({ mensagem: 'Conta criada com sucesso !!' });
+            }
+        }, 1500);
+    });
 }
