1efddb52b3efc16d17a26d7f61659e03d428673d
BrayanMateus5 <mateusbrayan73@gmail.com>
2026-06-23 12:57:38 -0300

    refactor(register): finalize JSX structure and rename CSS classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Navigate, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
+import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,39 +1,47 @@
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { Password } from 'primereact/password';
-import logo from '../../assets/nexion-logo.png';
-import './Login.css';
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Password } from "primereact/password";
+import logo from "../../assets/nexion-logo.png";
+import "./Login.css";
 
+const login = () => {
+  return (
+    <div className="login-background-form">
+      {/*Lado da marca */}
+      <div className="brand-left-section">
+        <img src={logo} alt="Logo da Nexion" className="logo-image" />
+        <h1 className="brand-logo-name">Nexion</h1>
+      </div>
 
-export default function Login() {
-    return (
-        <div className="login-back-form">
+      {/*Lado do formulário */}
+      <div className="form-right-section">
+        <div className="p-inside-form">
+          <h2>Entrar</h2>
 
-            {/*Lado da marca */}
-            <div className="brand-left-section">
-                <img src={logo} alt="Logo da Nexion" className="logo-image" />
-                <h1 className="brand-logo-name">Nexion</h1>
-            </div>
+          <div className="p-inputs">
+            <label htmlFor="email">Email</label>
+            <InputText id="email" type="text" placeholder="Digite seu email" />
+          </div>
 
-            {/*Lado do formulário */}
-            <div className="form-right-section">
-                <div className="p-interna-form">
-                    <h2>Entrar</h2>
+          <div className="p-inputs">
+            <label htmlFor="password">Senha</label>
+            <Password
+              id="password"
+              placeholder="Digite sua senha"
+              feedback={false}
+              toggleMask
+            />
+          </div>
 
-                    <div className="p-inputs">
-                        <label htmlFor="email">Email</label>
-                        <InputText id="email" type="text" placeholder="Digite seu email" />
-                    </div>
+          <Button label="Entrar" className="nexion-btn" />
 
-                    <div className="p-inputs">
-                        <label htmlFor="password">Senha</label>
-                        <Password id="password" placeholder="Digite sua senha" feedback={false} toggleMask />
-                    </div>
-
-                    <Button label="Entrar" className="nexion-btn" />
-
-                </div>
-            </div>
+          <p className="link-register">
+            Não possui conta ? <a href="/register">Cadastre-se</a>
+          </p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
+
+export default login;
